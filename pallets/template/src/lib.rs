ccfbeedbac641a77802680cc1c65e4854ac8dc53
Anish Chittigidde <57410583+anish63342@users.noreply.github.com>
2021-08-18 07:02:57 +0530

    Update lib.rs

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -83,6 +83,7 @@ pub mod pallet {
 		fn buy_product(
 			origin: OriginFor<T>,
 			productid: Vec<u8>,
+			_productname: Vec<u8>
 		) -> DispatchResultWithPostInfo {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
