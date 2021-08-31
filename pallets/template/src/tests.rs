cf590721a47d9c1f433e91816a2134b2054e871c
Anish Chittigidde <57410583+anish63342@users.noreply.github.com>
2021-08-31 22:25:06 +0530

    Update tests.rs

--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -5,17 +5,17 @@ use frame_support::{assert_ok, assert_noop};
 fn it_stores_correctly() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::store_product(Origin::signed(1),CF4865oY,iphone12,80000,Anish C));
+		assert_ok!(TemplateModule::sell_product(Origin::signed(1),CF4865oY,iphone12,80000,Anish C));
 		// Read pallet storage and assert an expected result.
 		assert_eq!(TemplateModule::proof(), Some(CF4865oY,iphone12));
 
-		assert_ok!(TemplateModule::store_product(Origin::signed(1),HG9666kJ,apple macbook air,94000,AnishCY));
+		assert_ok!(TemplateModule::sell_product(Origin::signed(1),HG9666kJ,apple macbook air,94000,AnishCY));
 		assert_eq!(TemplateModule::proof(), Some(HG9666kJ,apple macbook air));
 
-		assert_ok!(TemplateModule::store_product(Origin::signed(1),BD5400eL,nike airmax11,14000,Shinichi K));
+		assert_ok!(TemplateModule::sell_product(Origin::signed(1),BD5400eL,nike airmax11,14000,Shinichi K));
 		assert_eq!(TemplateModule::proof(), Some(BD5400eL,nike airmax11));
 
-		assert_ok!(TemplateModule::store_product(Origin::signed(1),AS89881xW, yonex nanoray 18i,3000,Silver Bullet));
+		assert_ok!(TemplateModule::sell_product(Origin::signed(1),AS89881xW, yonex nanoray 18i,3000,Silver Bullet));
 		assert_eq!(TemplateModule::proof(), Some(AS89881xW, yonex nanoray 18i));
 	});
 }
